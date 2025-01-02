94316d9f618ea5115cdbbea29b55319d5cd0c488
shubham-sharma-988 <viralclips988@gmail.com>
2025-01-02 15:35:47 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,13 @@
+console.log("2" > 1 )
+console.log("02" > 1)
+
+console.log(null > 0);
+console.log(null == 0 )
+console.log(null >= 0);  // when we are using the 
+// relational opereator than it convert null into 0 
+// where as null not change to 0 when using ==
+
+//  ===
+
+console.log("2" == 2)  // true here it not check the datatype
+console.log("2" === 2)// false here datatypes checks
