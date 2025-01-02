40d08f3a7544ee3a2a7b9a72a2ccaa27ea3540e0
shubham-sharma-988 <viralclips988@gmail.com>
2025-01-02 14:00:52 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score=33
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber =Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+// "33"=> 33
+// "33abc"=>NaN
+// true =>1 ; false=>0
+
+let isLoggedIn=0
+
+let booleanchk=Boolean(isLoggedIn)
+console.log(booleanchk);
+
+
+// 1 or any no. except 0 => true ; 0 =>false
+// ""=> false ; "lsijf"=>true
+
+
+let someno=33
+let stringno=String(someno)
+console.log(stringno);
