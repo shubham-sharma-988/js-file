e63df971afda4a8427976e64fb1382858cc95d27
shubham-sharma-988 <viralclips988@gmail.com>
2025-01-02 13:43:51 +0000

    variable and datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3)  // we are useing nodejs ,not browser
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Shubham")
+
+let name="Rahul"
+let age=18
+let isLoggedIn=false
+let state;
+
+// number=>2 to the power 53
+// bigint
+// string=> "" or ''
+// boolean => true or false
+// undefined =>
+// symbol => unique
+
+
+//  object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  // object
